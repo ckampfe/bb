055504c959520cf0903b316e4cd6fec098402090
Clark Kampfe <clark.kampfe@gmail.com>
2024-12-26 18:30:13 -0600

    peer protocol encoder and roundtrip tests

--- src/peer/protocol.rs
+++ src/peer/protocol.rs
@@ -1,5 +1,5 @@
 use crate::torrent::Pieces;
-use bytes::{Buf, BytesMut};
+use bytes::{Buf, BufMut, BytesMut};
 use thiserror::Error;
 use tokio::io::{AsyncReadExt, AsyncWriteExt, BufWriter};
 use tokio::net::TcpStream;
@@ -18,8 +18,10 @@ pub(super) enum Error {
 }
 
 const ONE_MB: usize = 1048576;
-const MAX: usize = 8 * ONE_MB;
+const FRAME_RECEIVE_MAX: usize = 8 * ONE_MB;
 
+/// A marker type on which to implement `Decoder` and `Encoder`.
+/// We then pass this type to `FramedRead` and `FramedWrite`
 pub(super) struct PeerProtocol {}
 
 impl PeerProtocol {
@@ -51,13 +53,19 @@ impl Decoder for PeerProtocol {
 
         let mut length_bytes = [0u8; 4];
         length_bytes.copy_from_slice(&src[..4]);
-        let length = u32::from_le_bytes(length_bytes) as usize;
+        let length = u32::from_be_bytes(length_bytes) as usize;
 
-        if length > MAX {
+        // basically, we don't want to get DDoS'd,
+        // so impose *some* limit here.
+        if length > FRAME_RECEIVE_MAX {
             return Err(std::io::Error::new(
-                std::io::ErrorKind::InvalidData,
+                std::io::ErrorKind::FileTooLarge,
                 format!("Frame of length {} is too large.", length),
             ));
+        }
+
+        if length == 0 {
+            return Ok(Some(Frame::Keepalive));
         }
 
         if src.len() < 4 + length {
@@ -104,19 +112,24 @@ impl Decoder for PeerProtocol {
             Self::PIECE => {
                 let index = rest.get_u32();
                 let begin = rest.get_u32();
-                let block = rest.chunk().to_owned();
+                let block = rest.to_vec();
 
-                // todo this is probably a bug
                 Ok(Some(Frame::Piece {
                     index,
                     begin,
                     block,
                 }))
             }
-            Self::CANCEL => Err(std::io::Error::new(
-                std::io::ErrorKind::Unsupported,
-                "cancel messages are not yet implemented",
-            )),
+            Self::CANCEL => {
+                let index = rest.get_u32();
+                let begin = rest.get_u32();
+                let length = rest.get_u32();
+                Ok(Some(Frame::Cancel {
+                    index,
+                    begin,
+                    length,
+                }))
+            }
             _ => Err(std::io::Error::new(
                 std::io::ErrorKind::Unsupported,
                 "received unknown message type",
@@ -129,10 +142,150 @@ impl Encoder<Frame> for PeerProtocol {
     type Error = std::io::Error;
 
     fn encode(&mut self, item: Frame, dst: &mut BytesMut) -> Result<(), Self::Error> {
-        todo!()
+        match item {
+            Frame::Keepalive => {
+                let len: u32 = 0;
+                let len_slice = [0, 0, 0, 0];
+
+                dst.reserve(len_slice.len() + len as usize);
+
+                dst.extend_from_slice(&len_slice);
+            }
+            Frame::Choke => {
+                let len: u32 = 1;
+                let len_slice = len.to_be_bytes();
+
+                dst.reserve(len_slice.len() + len as usize);
+
+                dst.extend_from_slice(&len_slice);
+                dst.put_u8(Self::CHOKE);
+            }
+            Frame::Unchoke => {
+                let len: u32 = 1;
+                let len_slice = len.to_be_bytes();
+
+                dst.reserve(len_slice.len() + len as usize);
+
+                dst.extend_from_slice(&len_slice);
+                dst.put_u8(Self::UNCHOKE);
+            }
+            Frame::Interested => {
+                let len: u32 = 1;
+                let len_slice = len.to_be_bytes();
+
+                dst.reserve(len_slice.len() + len as usize);
+
+                dst.extend_from_slice(&len_slice);
+                dst.put_u8(Self::INTERESTED);
+            }
+            Frame::NotInterested => {
+                let len = 1u32;
+                let len_slice = len.to_be_bytes();
+
+                dst.reserve(len_slice.len() + len as usize);
+                dst.extend_from_slice(&len_slice);
+                dst.put_u8(Self::NOT_INTERESTED);
+            }
+            Frame::Have { index } => {
+                let index_slice = index.to_be_bytes();
+                let len: u32 = 1 + index_slice.len() as u32;
+                let len_slice = len.to_be_bytes();
+
+                dst.reserve(len_slice.len() + len as usize);
+
+                dst.extend_from_slice(&len_slice);
+                dst.put_u8(Self::HAVE);
+                dst.extend_from_slice(&index_slice);
+            }
+            Frame::Bitfield { pieces } => {
+                let pieces_slice = pieces.as_raw_slice();
+                let len: u32 = 1 + pieces_slice.len() as u32;
+                let len_slice = len.to_be_bytes();
+
+                dst.reserve(len_slice.len() + len as usize);
+
+                dst.extend_from_slice(&len_slice);
+                dst.put_u8(Self::BITFIELD);
+                dst.extend_from_slice(pieces_slice);
+            }
+            Frame::Request {
+                index,
+                begin,
+                length,
+            } => {
+                let index_slice = index.to_be_bytes();
+                let begin_slice = begin.to_be_bytes();
+                let length_slice = length.to_be_bytes();
+
+                let len: u32 = 1
+                    + index_slice.len() as u32
+                    + begin_slice.len() as u32
+                    + length_slice.len() as u32;
+
+                let len_slice = len.to_be_bytes();
+
+                dst.reserve(len_slice.len() + len as usize);
+
+                dst.extend_from_slice(&len_slice);
+                dst.put_u8(Self::REQUEST);
+                dst.extend_from_slice(&index_slice);
+                dst.extend_from_slice(&begin_slice);
+                dst.extend_from_slice(&length_slice);
+            }
+            Frame::Piece {
+                index,
+                begin,
+                block,
+            } => {
+                let index_slice = index.to_be_bytes();
+                let begin_slice = begin.to_be_bytes();
+
+                let len: u32 = 1
+                    + index_slice.len() as u32
+                    + begin_slice.len() as u32
+                    + u32::try_from(block.len())
+                        .expect("block must have a length that can fit into a u32");
+                let len_slice = len.to_be_bytes();
+
+                dst.reserve(len_slice.len() + len as usize);
+
+                dst.extend_from_slice(&len_slice);
+                dst.put_u8(Self::PIECE);
+                dst.extend_from_slice(&index_slice);
+                dst.extend_from_slice(&begin_slice);
+                dst.extend_from_slice(&block);
+            }
+            Frame::Cancel {
+                index,
+                begin,
+                length,
+            } => {
+                let index_slice = index.to_be_bytes();
+                let begin_slice = begin.to_be_bytes();
+                let length_slice = length.to_be_bytes();
+
+                let len: u32 = 1
+                    + index_slice.len() as u32
+                    + begin_slice.len() as u32
+                    + length_slice.len() as u32;
+                let len_slice = len.to_be_bytes();
+
+                dst.reserve(len_slice.len() + len as usize);
+
+                dst.extend_from_slice(&len_slice);
+                dst.put_u8(Self::CANCEL);
+                dst.extend_from_slice(&index_slice);
+                dst.extend_from_slice(&begin_slice);
+                dst.extend_from_slice(&length_slice);
+            }
+        }
+
+        Ok(())
     }
 }
 
+#[derive(Debug)]
+#[cfg_attr(test, derive(PartialEq))]
 pub(super) enum Frame {
     Keepalive,
     Choke,
@@ -160,4 +313,217 @@ pub(super) enum Frame {
         begin: u32,
         length: u32,
     },
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::SinkExt;
+    use rand::{thread_rng, Rng};
+    use tokio_stream::StreamExt;
+    use tokio_util::codec::{FramedRead, FramedWrite};
+
+    use crate::torrent::Pieces;
+
+    use super::{Frame, PeerProtocol};
+
+    #[tokio::test]
+    async fn roundtrips_keepalive() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+        writer.send(Frame::Keepalive).await.unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(reader.next().await.unwrap().unwrap(), Frame::Keepalive);
+    }
+
+    #[tokio::test]
+    async fn roundtrips_choke() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+        writer.send(Frame::Choke).await.unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(reader.next().await.unwrap().unwrap(), Frame::Choke);
+    }
+
+    #[tokio::test]
+    async fn roundtrips_unchoke() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+        writer.send(Frame::Unchoke).await.unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(reader.next().await.unwrap().unwrap(), Frame::Unchoke);
+    }
+
+    #[tokio::test]
+    async fn roundtrips_interested() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+        writer.send(Frame::Interested).await.unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(reader.next().await.unwrap().unwrap(), Frame::Interested);
+    }
+
+    #[tokio::test]
+    async fn roundtrips_not_interested() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+        writer.send(Frame::NotInterested).await.unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(reader.next().await.unwrap().unwrap(), Frame::NotInterested);
+    }
+
+    #[tokio::test]
+    async fn roundtrips_have() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+
+        let index = rand::random();
+
+        writer.send(Frame::Have { index }).await.unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(reader.next().await.unwrap().unwrap(), Frame::Have { index });
+    }
+
+    #[tokio::test]
+    async fn roundtrips_bitfield() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+
+        let mut rng = thread_rng();
+
+        let raw_pieces_length = rng.gen_range(0..500);
+        let mut pieces_raw = vec![0; raw_pieces_length];
+        rng.fill(&mut pieces_raw[..]);
+
+        let pieces = Pieces::from_slice(&pieces_raw);
+
+        writer
+            .send(Frame::Bitfield {
+                pieces: pieces.clone(),
+            })
+            .await
+            .unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(
+            reader.next().await.unwrap().unwrap(),
+            Frame::Bitfield { pieces }
+        );
+    }
+
+    #[tokio::test]
+    async fn roundtrips_request() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+
+        let index = rand::random();
+        let begin = rand::random();
+        let length = rand::random();
+
+        writer
+            .send(Frame::Request {
+                index,
+                begin,
+                length,
+            })
+            .await
+            .unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(
+            reader.next().await.unwrap().unwrap(),
+            Frame::Request {
+                index,
+                begin,
+                length
+            }
+        );
+    }
+
+    #[tokio::test]
+    async fn roundtrips_piece() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+
+        let mut rng = thread_rng();
+
+        let index = rand::random();
+        let begin = rand::random();
+        let block_length = rng.gen_range(0..500);
+        let mut block = vec![0; block_length];
+        rng.fill(&mut block[..]);
+
+        writer
+            .send(Frame::Piece {
+                index,
+                begin,
+                block: block.clone(),
+            })
+            .await
+            .unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(
+            reader.next().await.unwrap().unwrap(),
+            Frame::Piece {
+                index,
+                begin,
+                block
+            }
+        );
+    }
+
+    #[tokio::test]
+    async fn roundtrips_cancel() {
+        let buf = vec![];
+        let mut writer = FramedWrite::new(buf, PeerProtocol::new());
+
+        let index = rand::random();
+        let begin = rand::random();
+        let length = rand::random();
+
+        writer
+            .send(Frame::Cancel {
+                index,
+                begin,
+                length,
+            })
+            .await
+            .unwrap();
+        let out = writer.get_ref();
+
+        let mut reader = FramedRead::new(&out[..], PeerProtocol::new());
+
+        assert_eq!(
+            reader.next().await.unwrap().unwrap(),
+            Frame::Cancel {
+                index,
+                begin,
+                length
+            }
+        );
+    }
 }
